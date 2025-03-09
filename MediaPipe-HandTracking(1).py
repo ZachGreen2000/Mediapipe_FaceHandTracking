83d3de78c8553c54abdadfe1ea8b31a1d2f55a54
GodKiller-21 <147402682+ZachGreen2000@users.noreply.github.com>
2025-03-09 12:22:05 +0000

    adding images for explosion set up

--- MediaPipe-HandTracking(1).py
+++ MediaPipe-HandTracking(1).py
@@ -22,7 +22,18 @@ class GestureRecogniser():
                                                running_mode=self.VisionRunningMode.LIVE_STREAM,
                                                  result_callback=self.identifyGesture)
       self.recognizer = self.GestureRecognizer.create_from_options(options)
-
+      # array of explosion images
+      self.explosion_images = [
+        cv2.imread("explosion1.png"),
+        cv2.imread("explosion2.pmg"),
+        cv2.imread("explosion3.png"),
+        cv2.imread("explosion4.png"),
+        cv2.imread("explosion5.png")
+      ]
+      self.explosion_index  = 0
+      self.explosion_active = False
+      self.explosion_position = None
+      self.explosion_start = None
     def identifyGesture(self, result, output_image: mp.Image, timestamp_ms: int):
        if result.gestures:
         #get confidence of gesture
@@ -43,8 +54,8 @@ class FaceRecogniser():
       self.gesture_timestamp_face = 0
 
     def detect_movement(self, image):
-      pygame.mixer.init()
-      talking_sound = pygame.mixer.Sound("talking.wav")
+      pygame.mixer.init() #sets up sound with pygame
+      talking_sound = pygame.mixer.Sound("talking.wav") #stores sound effect
       results = self.face_mesh.process(image)
       if not results.multi_face_landmarks:
         return None
@@ -56,8 +67,8 @@ class FaceRecogniser():
       if lip_distance > 0.02:
         self.face_gesture = "Talking!"
         self.gesture_timestamp_face = time.time()
-        if not pygame.mixer.get_busy():
-          talking_sound.play()
+        if not pygame.mixer.get_busy():#makes sure sound isnt already playing
+          talking_sound.play()#plays sound
       #to detect head shaking
       current_nose = face_landmarks.landmark[1].x
       if self.initial_nose is not None and abs(current_nose - self.initial_nose) > 0.05:
